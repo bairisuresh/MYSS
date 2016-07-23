83f2a65156419998c478af2d9f0cc37011158410
Sreevenkatesh <sreevenkateshn@gmail.com>
2016-07-24 01:38:38 +0530

    ntn

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from './HeaderComponent'
-
+import Body from './BodyComponent'
+require('styles//App.scss');
 const App = () => (
-  <div id="myssApp">
+  <div id="myssApp" className="app-component">
     <Header />
+    <Body/>
   </div>
 )
 
---
+++ src/components/BodyComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//Body.scss');
+
+class BodyComponent extends React.Component {
+  render() {
+    return (
+      <div className="body-component">
+       
+      </div>
+    );
+  }
+}
+
+BodyComponent.displayName = 'BodyComponent';
+
+// Uncomment properties you need
+// BodyComponent.propTypes = {};
+// BodyComponent.defaultProps = {};
+
+export default BodyComponent;
