2571cd297f49ec0f278f31d6304e67020ae9e1ee
suresh bairi <suresh bairi>
2016-07-31 20:52:52 +0530

    inbox component

--- src/components/MailBoxComponent.js
+++ src/components/MailBoxComponent.js
@@ -1,17 +1,99 @@
 'use strict';
 
 import React from 'react';
-
+import ActiveItem from './AiitemComponent';
+import Image from './ImageBgComponent';
 require('styles//MailBox.scss');
 
 class MailBoxComponent extends React.Component {
-  render() {
-    return (
-      <div className="mailbox-component">
-        Please edit src/components///MailBoxComponent.js to update this component!
-      </div>
-    );
-  }
+      constructor(props,context){
+          super(props,context);
+          this.state = {
+           content: [{
+              image1 : "group.png",
+              type : "Investment Access Report",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsum Dolor Spec",
+              image2 : "arrow-r.svg"
+          },{
+              image1 : "group.png",
+              type : "Accounting Summary",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsum",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsum",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Dolor specturuem",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsu",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsum",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsu",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsum",
+              image2 : "arrow-r.svg"
+          },
+          {
+              image1 : "group.png",
+              type : "Lorem Ipsum",
+              image2 : "arrow-r.svg"
+          }]
+        }
+      }
+    render() {
+    //console.log("state is: "+JSON.stringify(this.getState()));
+      return (
+        <div className="mailbox-component">
+  		<div className="mailbox-header">
+  			<div>Inbox</div>
+  			<hr/>
+  		</div>
+  		<div className="mailbox-body container-fluid">
+  		{
+  			this.state.content.map(function(data){
+  				return(
+            <div className="mailbox-img-component row">
+      			<div className="col-md-2"><Image image={data.image1}/></div>
+      		  	<div className="col-md-8">
+      		  		{data.type}
+      		  	</div>
+      		  	<div className="col-md-2">
+      		  		<Image image={data.image2}/>
+      		  	</div>
+            </div>
+          )
+  			})
+
+  		}
+  		</div>
+        </div>
+      );
+    }
 }
 
 MailBoxComponent.displayName = 'MailBoxComponent';
