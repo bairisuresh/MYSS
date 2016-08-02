8d91f3c6bc413f4845e9ae56f0f59bb3d0c94dc7
bairisuresh <bairi.suresh@gmail.com>
2016-08-02 19:43:55 +0530

    Aiitem test case

--- test/components/ListComponentTest.js
+++ test/components/ListComponentTest.js
@@ -9,7 +9,7 @@ import createComponent from 'helpers/shallowRenderHelper';
 
 import ListComponent from 'components//ListComponent.js';
 
-xdescribe('ListComponent', () => {
+describe('ListComponent', () => {
   let component;
 
   beforeEach(() => {
