5a70ae14b5e9ebb85658e9385a913c307cd1436e
sreevenkateshn <sreevenkateshn@gmail.com>
2016-08-05 20:42:40 +0530

    redux

--- src/stores/index.js
+++ src/stores/index.js
@@ -16,4 +16,7 @@ module.exports = function(initialState) {
   return store
 };
 store.dispatch(navigateToHome());
-store.dispatch(navigateToGrid());
+store.dispatch(navigateToGrid({id : "11",
+            activityName : "3-12 Top Holding IV pm SLB1",
+            activityDate : "19/06/16",
+          }));
