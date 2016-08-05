a2a3885feb59f1e32660f5758b70c12538c82d03
bairisuresh <bairi.suresh@gmail.com>
2016-08-05 15:59:27 +0530

    carousal changes

--- src/components/TypesFsComponent.js
+++ src/components/TypesFsComponent.js
@@ -3,12 +3,28 @@
 import React from 'react';
 
 require('styles//TypesFs.scss');
-
+var Carousel = require('nuka-carousel');
 class TypesFsComponent extends React.Component {
   render() {
     return (
       <div className="typesfs-component">
-        <img src="/images/home-accordion.png" width="100%" height="100%"/>
+         <Carousel slidesToShow={3}>
+            <Carousel className="sample">
+                <img src="images/accounting.png"/>
+                <img src="images/custody.png"/>
+                <img src="images/ifs.png" />
+            </Carousel>
+            <img src="images/custody.png" />
+            <Carousel className="sample">
+                <img src="images/ifs.png" />
+                <img src="images/irk.png" />
+            </Carousel>
+            <Carousel className="sample">
+                <img src="images/irk.png" />
+                <img src="images/accounting.png"/>
+                <img src="images/ifs.png" />
+            </Carousel>
+        </Carousel>
       </div>
     );
   }
